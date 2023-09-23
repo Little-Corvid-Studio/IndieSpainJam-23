2e34d48118eca496a0b3fe9e60752d5c5ee830a4
A. Rubio <ampxrubio@gmail.com>
2023-09-23 17:10:20 +0100

    constelaciones terminadas

--- Assets/_Project/Scripts/UI/ConstellationList.cs
+++ Assets/_Project/Scripts/UI/ConstellationList.cs
@@ -16,6 +16,9 @@ public enum ConstellationNames
     AQUARIUS,
     PISCIS,
 
+    MAYOR,
+    MINOR,
+
     NUM_CONSTELLATIONS
 }
 
@@ -113,16 +116,36 @@ public class ConstellationList
 
                 break;
             case ConstellationNames.VIRGO:
-                data.numEstrellas = 9;
-                data.conexiones = new bool[12, 12];
-                for (int i = 0; i < 12; i++)
+                data.numEstrellas = 8;
+                data.conexiones = new bool[8, 8];
+                for (int i = 0; i < 8; i++)
                 {
-                    for (int j = 0; j < 12; j++)
+                    for (int j = 0; j < 8; j++)
                     {
                         data.conexiones[i, j] = false;
                     }
                 }
 
+
+                data.conexiones[0, 1] = true;
+                data.conexiones[1, 0] = true;
+
+                data.conexiones[2, 1] = true;
+                data.conexiones[1, 2] = true;
+
+                data.conexiones[2, 3] = true;
+                data.conexiones[3, 2] = true;
+
+                data.conexiones[3, 4] = true;
+                data.conexiones[4, 3] = true;
+                data.conexiones[1, 4] = true;
+                data.conexiones[4, 1] = true;
+
+                data.conexiones[7, 4] = true;
+                data.conexiones[4, 7] = true;
+
+                data.conexiones[3, 6] = true;
+                data.conexiones[6, 3] = true;
                 break;
             case ConstellationNames.CANCER:
                 data.numEstrellas = 5;
@@ -247,6 +270,36 @@ public class ConstellationList
 
 
                 break;
+
+
+            case ConstellationNames.AQUARIUS:
+                data.numEstrellas = 9;
+                data.conexiones = new bool[9, 9];
+                for (int i = 0; i < 9; i++)
+                {
+                    for (int j = 0; j < 9; j++)
+                    {
+                        data.conexiones[i, j] = false;
+                    }
+                }
+
+                data.conexiones[0, 1] = true;
+                data.conexiones[1, 0] = true;
+                data.conexiones[0, 6] = true;
+                data.conexiones[6, 0] = true;
+                data.conexiones[2, 1] = true;
+                data.conexiones[1, 2] = true;
+                data.conexiones[0, 3] = true;
+                data.conexiones[3, 0] = true;
+                data.conexiones[3, 5] = true;
+                data.conexiones[5, 3] = true;
+                data.conexiones[5, 4] = true;
+                data.conexiones[4, 5] = true;
+                data.conexiones[5, 7] = true;
+                data.conexiones[7, 5] = true;
+                data.conexiones[8, 4] = true;
+                data.conexiones[4, 8] = true;
+                break;
             case ConstellationNames.CAPRICORNIO:
                 data.numEstrellas = 5;
                 data.conexiones = new bool[5, 5];
@@ -307,6 +360,68 @@ public class ConstellationList
 
                 data.conexiones[4, 0] = true;
                 data.conexiones[0, 4] = true;
+
+                break;
+
+            case ConstellationNames.MAYOR:
+                data.numEstrellas = 6;
+                data.conexiones = new bool[6, 6];
+                for (int i = 0; i < 6; i++)
+                {
+                    for (int j = 0; j < 6; j++)
+                    {
+                        data.conexiones[i, j] = false;
+                    }
+                }
+
+                data.conexiones[0, 2] = true;
+                data.conexiones[2, 0] = true;
+
+                data.conexiones[1, 2] = true;
+                data.conexiones[2, 1] = true;
+                
+                data.conexiones[1, 3] = true;
+                data.conexiones[3, 1] = true;
+
+                data.conexiones[4, 3] = true;
+                data.conexiones[3, 4] = true;
+
+                data.conexiones[1,5] = true;
+                data.conexiones[5, 1] = true;
+
+                data.conexiones[4, 5] = true;
+                data.conexiones[5, 4] = true;
+
+                break;
+
+            case ConstellationNames.MINOR:
+                data.numEstrellas = 6;
+                data.conexiones = new bool[6, 6];
+                for (int i = 0; i < 6; i++)
+                {
+                    for (int j = 0; j < 6; j++)
+                    {
+                        data.conexiones[i, j] = false;
+                    }
+                }
+
+                data.conexiones[0, 2] = true;
+                data.conexiones[2, 0] = true;
+
+                data.conexiones[1, 2] = true;
+                data.conexiones[2, 1] = true;
+
+                data.conexiones[1, 3] = true;
+                data.conexiones[3, 1] = true;
+
+                data.conexiones[4, 3] = true;
+                data.conexiones[3, 4] = true;
+
+                data.conexiones[1, 5] = true;
+                data.conexiones[5, 1] = true;
+
+                data.conexiones[4, 5] = true;
+                data.conexiones[5, 4] = true;
 
                 break;
         }
